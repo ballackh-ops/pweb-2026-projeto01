91ec7557062581a9b83abc5d55bf8d88c58656d4
Hugo <ballack.h@escolar.ifrn.edu.br>
2026-05-26 13:56:10 -0300

    teste no template

--- projeto/views.py
+++ projeto/views.py
@@ -6,3 +6,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, "index.html")
 
+def index(request):
+    dados_usuario = {"nome": "Michael Douglas", "idade": 23}
+    return render(request, "index.html", dados_usuario)
