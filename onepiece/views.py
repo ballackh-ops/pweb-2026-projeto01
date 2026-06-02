7985589a62ba27d8280b19364ed1dc3cd04147f2
Nivaldo <bezerranivaldo70@gmail.com>
2026-06-02 11:20:16 -0300

    eliminando o mal pela raiz

--- onepiece/views.py
+++ onepiece/views.py
@@ -2,7 +2,3 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "onepiece/index.html")
-
-def index(request):
-    dados_usuario = {"nome": "Michael Douglas", "idade": 23}
-    return render(request, "index.html", dados_usuario)
