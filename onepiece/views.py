be3a717a700081a17db00ca04f4e4631db4182ca
Nivaldo <bezerranivaldo70@gmail.com>
2026-06-02 11:28:49 -0300

    começando o app

--- onepiece/views.py
+++ onepiece/views.py
@@ -2,3 +2,16 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "onepiece/index.html")
+
+lista_usuario = [
+        {"Nome": "Iñaki Godoy", "Idade": 19, "Cidade": "Cidade do México"},
+        {"Nome": "Emily Rudd", "Idade": 19, "Cidade": "Spring Valley"},
+        {"Nome": "Jacob Gibson", "Idade": 19, "Cidade": "Denver"},
+        {"Nome": "Taz Skylar", "Idade": 19, "Cidade": "Tenerife"},
+        {"Nome": "Mackenyu", "Idade": 19, "Cidade": "Little Tokyo"},
+    ]
+
+    context = {
+        "usuarios": lista_usuario,
+    }
+    return render(request, "onepiece/index.html", context)
