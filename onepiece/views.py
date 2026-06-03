9d5647e354c87d142a40c9837cf94f4bdc0d6cfb
Hugo <ballack.h@escolar.ifrn.edu.br>
2026-06-03 10:21:36 -0300

    testando com com uma nav

--- onepiece/views.py
+++ onepiece/views.py
@@ -7,10 +7,10 @@ def usuarios(request):
 
     lista_usuario = [
             {"nome": "Iñaki Godoy", "idade": 19, "cidade": "Cidade do México"},
-            {"nome": "Emily Rudd", "idade": 19, "cidade": "Spring Valley"},
-            {"nome": "Jacob Gibson", "idade": 19, "cidade": "Denver"},
-            {"nome": "Taz Skylar", "idade": 19, "cidade": "Tenerife"},
-            {"nome": "Mackenyu", "idade": 19, "cidade": "Little Tokyo"},
+            # {"nome": "Emily Rudd", "idade": 19, "cidade": "Spring Valley"},
+            # {"nome": "Jacob Gibson", "idade": 19, "cidade": "Denver"},
+            # {"nome": "Taz Skylar", "idade": 19, "cidade": "Tenerife"},
+            # {"nome": "Mackenyu", "idade": 19, "cidade": "Little Tokyo"},
         ]
     
     context = {"usuarios": lista_usuario,}
